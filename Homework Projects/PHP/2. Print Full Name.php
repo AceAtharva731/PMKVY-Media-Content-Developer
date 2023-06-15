3c2a9204d790df652c046a42eedc7925df23ccda
Atharva Pardeshi <114866146+AceAtharva731@users.noreply.github.com>
2023-06-15 12:27:56 +0530

    Add files via upload

---
+++ Homework Projects/PHP/2. Print Full Name.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    $fname = "Atharva";
+    $mname = "Naresh";
+    $lname = "Pardeshi";
+
+    echo "First name: ", $fname;
+    echo "<br>";
+    echo "Middle name: ", $mname;
+    echo "<br>";
+    echo "Last name: ", $lname;
+    ?>
+
+</body>
+
+</html>
